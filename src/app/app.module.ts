ba9900a572a859492d7d0b11030ee0b4b1199dcc
jorgernes <sosapcsmx@gmail.com>
2022-06-28 23:38:47 -0500

    complete componentes en paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './share/header/header.component';
 import { FooterComponent } from './share/footer/footer.component';
 import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
+import { AboutComponent } from './paginas/about/about.component';
+import { ItemComponent } from './paginas/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
